4c950451ea421ed9764c6940984f1c7b732b8998
Siem Debesay <siem3500@gmail.com>
2023-11-19 19:35:29 -0700

    added link to Lida application for futher analysis

--- worldMap/src/App.js
+++ worldMap/src/App.js
@@ -6,7 +6,11 @@ import CompanyButtonList from "./button";
 function App() {
   return (
     <div className="App">
-      <div style={{fontSize: "xxx-large", fontWeight:"bold",  color: 'white',}}> World Map</div>
+      <div style={{fontSize: "xxx-large", fontWeight:"bold", color: 'white',}}> World Map</div>
+      <a href="https://siemd2-lida-exp-app-qqa1k2.streamlit.app/" 
+         style={{ color: '#E2AEFF', fontWeight: 'bold', fontSize: 'large' }}>
+         Ask Lida !
+      </a>
       <WorldMap />
     </div>
   );
